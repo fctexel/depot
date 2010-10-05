1ad83bb968e69f13baff9a304679a28a7dcd1771
Gert Folkerts <info@fctexel.com>
2010-10-05 21:56:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'b1c24c99041c5948f026988cdfd6efa840dbdd2b7f4949305939aa8c765541164ce480cf7526430591d5794af82c98ec7291dfe8f3c59aa68213986c01df6e03'
