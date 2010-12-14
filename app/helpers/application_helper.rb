b1c3cc3631672c4bfb23901aeb12e9de037b9e3e
dovadi <frank.oxener@gmail.com>
2010-12-14 21:35:25 +0100

    Fix styling of order form and some cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-    def hidden_div_if(condition, attributes = {}, &block)
-      if condition
-        attributes["style"] = "display: none"
-      end
-      content_tag("div", attributes, &block)
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes["style"] = "display: none"
     end
+    content_tag("div", attributes, &block)
   end
+end
