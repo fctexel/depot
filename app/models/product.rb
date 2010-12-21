e9d850546afeb7242eef3c17e8bc02825fe78b7c
Gert Folkerts <info@fctexel.com>
2010-12-21 08:56:37 +0100

    h12 update

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
 
   default_scope :order => 'title'
   has_many :line_items
+  has_many :orders, :through => :line_items
 
   validates :image_url, :format => {:with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG or PNG image.' }
   validates :title, :description, :image_url, :presence =>true
