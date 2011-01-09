eed4cbe8e8acea36a86269305b1c269cdcae1d32
Gert Folkerts <info@fctexel.com>
2011-01-09 15:17:39 +0100

    update depot H13

--- test/functional/notifier_test.rb
+++ test/functional/notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class NotifierTest < ActionMailer::TestCase
   test "order_received" do
-    mail = Notifier.order_received
-    assert_equal "Order received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = Notifier.order_received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "order_shipped" do
-    mail = Notifier.order_shipped
-    assert_equal "Order shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = Notifier.order_shipped(orders(:one))
+    assert_equal "Pragmatic Store Order shipped", mail.subject
+    assert_equal ["dave@example.com"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /<td>1&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/,mail.body.encoded
   end
 
 end
